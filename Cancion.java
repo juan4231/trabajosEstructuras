243b4c611b645680bd82b4488497c40c66e5a11f
juan4231 <jcpuentesl@Libertadores.edu.co>
2026-01-23 02:23:20 +0000

    se ajusto la clase cancion

---
+++ Cancion.java
@@ -0,0 +1,10 @@
+public class cancion{
+private String nombrecancion;
+private String artista;
+private String Genero;
+public cancion(String nombrecancion, String artista,String Genero) {
+    this.nombrecancion=nombrecancion;
+    this.artista=artista;
+    this.Genero=Genero;
+} 
+}
