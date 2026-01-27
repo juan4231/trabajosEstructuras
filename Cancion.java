1ba3f969ebf7f94bcf31d49df5c4769442f2e006
juan4231 <jcpuentesl@Libertadores.edu.co>
2026-01-27 01:31:56 +0000

    Cancion

--- Cancion.java
+++ Cancion.java
@@ -12,8 +12,19 @@ public String getnombrecancion(){
 }
 public void setnombrecancion(String nombrecancion){
     this.nombrecancion = nombrecancion;
-    
-
 }
+public String getartista(){
+    return artista;
+}
+public void setartista(String artista){
+    this.artista = artista;
+}
+public String getGenero(){
+    return Genero;
+}
+public void setGenero(String Genero){
+    this.Genero = Genero;
+}
+
 
 }
